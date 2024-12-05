6a3d8061146c6e5128054d52d035b699c286ba8e
Anders Tøgersen <54847232+Anders-Toegersen@users.noreply.github.com>
2024-12-05 15:44:38 +0100

    feat: solved day1

---
+++ Ato.AoC2024/Day1/HistorianHysteria.cs
@@ -0,0 +1,43 @@
+﻿namespace Ato.AoC2024.Day1;
+
+public class HistorianHysteria : IProblem
+{
+    public string Solve1(string input)
+    {
+        var lines = input
+            .Split(Environment.NewLine)
+            .Select(x => x.Split("   ").Select(int.Parse));
+
+        var left = lines
+            .Select(x => x.First())
+            .Order();
+
+        var right = lines
+            .Select(x => x.Last())
+            .Order();
+
+        var pairwiseSummmedDistances = left
+            .Zip(right)
+            .Sum(x => Math.Abs(x.First - x.Second));
+
+        return pairwiseSummmedDistances.ToString();
+    }
+
+    public string Solve2(string input)
+    {
+        var lines = input
+            .Split(Environment.NewLine)
+            .Select(x => x.Split("   ").Select(int.Parse));
+
+        var rightByCount = lines
+            .Select(x => x.Last())
+            .GroupBy(x => x)
+            .ToDictionary(x => x.Key, x => x.Count());
+
+        var similarityScore = lines
+            .Select(x => x.First())
+            .Sum(x => rightByCount.GetValueOrDefault(x) * x);
+
+        return similarityScore.ToString();
+    }
+}
---
+++ Ato.AoC2024/IProblem.cs
@@ -0,0 +1,8 @@
+﻿namespace Ato.AoC2024;
+
+public interface IProblem
+{
+    string Solve1(string input);
+
+    string Solve2(string input);
+}
