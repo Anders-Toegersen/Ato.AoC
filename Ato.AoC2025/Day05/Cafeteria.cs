16d07f9554ff15332e47dd6d5527a418efd5fe75
Anders Tøgersen <54847232+Anders-Toegersen@users.noreply.github.com>
2025-12-06 22:55:29 +0100

    feat: solved Day05

---
+++ Ato.AoC2025/Day05/Cafeteria.cs
@@ -0,0 +1,51 @@
+﻿namespace Ato.AoC2025.Day05;
+
+internal class Cafeteria : IProblem
+{
+    public string Solve1(string input)
+    {
+        var freshIds = input
+            .Split(Environment.NewLine + Environment.NewLine)[0]
+            .Split(Environment.NewLine)
+            .Select(line => line.Split('-'))
+            .Select(limits => (Lower: long.Parse(limits[0]), Upper: long.Parse(limits[1])));
+
+        var rottenIdsCount = input
+            .Split(Environment.NewLine + Environment.NewLine)[1]
+            .Split(Environment.NewLine)
+            .Select(long.Parse)
+            .Where(id => freshIds.Any(range => id >= range.Lower && id <= range.Upper))
+            .Count();
+
+        return rottenIdsCount.ToString();
+    }
+
+    public string Solve2(string input)
+    {
+        var freshIdsCount = input
+            .Split(Environment.NewLine + Environment.NewLine)[0]
+            .Split(Environment.NewLine)
+            .Select(line => line.Split('-'))
+            .Select(limits => (Lower: long.Parse(limits[0]), Upper: long.Parse(limits[1])))
+            .OrderBy(range => range.Lower)
+            .Aggregate(
+                new List<(long Lower, long Upper)>(), 
+                (mergedRanges, range) =>
+                {
+                    if (mergedRanges.Count == 0 || 
+                        mergedRanges[^1].Upper < range.Lower)
+                    {
+                        mergedRanges.Add(range);
+                        return mergedRanges;
+                    }
+
+                    mergedRanges[^1] = (mergedRanges[^1].Lower, Math.Max(mergedRanges[^1].Upper, range.Upper));
+
+                    return mergedRanges;
+                })
+            .Select(x => x.Upper - x.Lower + 1)
+            .Sum();
+
+        return freshIdsCount.ToString();
+    }
+}
